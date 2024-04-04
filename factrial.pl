128935eb6512af168cf8b6d5af88418bedf2bcf7
mirzabilal17 <147336832+mirzabilal17@users.noreply.github.com>
2024-04-04 09:58:34 +0530

    Update factrial.pl

--- factrial.pl
+++ factrial.pl
@@ -18,3 +18,22 @@ N1 is N-1,
 fact(N1,F1), 
 
 F is N*F1.
+
+
+
+
+
+
+
+
+move(1,X,Y,_) :-
+   write('Move top disk from '), write(X), write(' to '), write(Y), nl.
+move(N,X,Y,Z) :-
+   N>1,
+   M is N-1,
+   move(M,X,Z,Y),
+   move(1,X,Y,_),
+   move(M,Z,Y,X).
+
+tower_of_hanoi(N) :-
+   move(N, 'A', 'C', 'B').
